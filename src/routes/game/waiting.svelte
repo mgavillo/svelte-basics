67fec2e5735ca9c0411ff9a0ff8ed3f19f62d661
mgavillo <marie.gavillon@gmail.com>
2023-07-26 12:32:25 +0100

    testé

--- src/routes/game/waiting.svelte
+++ src/routes/game/waiting.svelte
@@ -4,20 +4,20 @@
   $: duration = 1000;
   $: easing = linear;
   $: options = { duration, easing, times: 2 };
-  function spin(node: any, options: any) {
-    const { times = 1 } = options;
-    return {
-      ...options,
-      // The value of t passed to the css method
-      // varies between zero and one during an "in" transition
-      // and between one and zero during an "out" transition.
-      css(t: any) {
-        // Svelte takes care of applying the easing function.
-        const degrees = 360 * times; // through which to spin
-        return `transform: rotate(${t * degrees}deg);`;
-      },
-    };
-  }
+  // function spin(node: any, options: any) {
+  //   const { times = 1 } = options;
+  //   return {
+  //     ...options,
+  //     // The value of t passed to the css method
+  //     // varies between zero and one during an "in" transition
+  //     // and between one and zero during an "out" transition.
+  //     css(t: any) {
+  //       // Svelte takes care of applying the easing function.
+  //       const degrees = 360 * times; // through which to spin
+  //       return `transform: rotate(${t * degrees}deg);`;
+  //     },
+  //   };
+  // }
 </script>
 
 <div
