74ab5c72d99270310d7b1b2ea3d2662d3cd036ae
mgavillo <marie.gavillon@gmail.com>
2023-07-26 12:14:42 +0100

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,49 +1,60 @@
-import preprocess from 'svelte-preprocess';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-import { execSync } from 'child_process';
-import adapter from '@sveltejs/adapter-vercel';
+// import preprocess from 'svelte-preprocess';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+// import { execSync } from 'child_process';
+// import adapter from '@sveltejs/adapter-vercel';
 
-function getVersion() {
-	try {
-		return execSync('git rev-parse --short HEAD').toString().trim();
-	} catch {
-		const timestamp = Date.now().toString();
-		console.error(
-			`could not get commit-hash to set a version id, falling back on timestamp ${timestamp}`
-		);
-		return timestamp;
-	}
-}
-const VERSION = getVersion();
+// function getVersion() {
+// 	try {
+// 		return execSync('git rev-parse --short HEAD').toString().trim();
+// 	} catch {
+// 		const timestamp = Date.now().toString();
+// 		console.error(
+// 			`could not get commit-hash to set a version id, falling back on timestamp ${timestamp}`
+// 		);
+// 		return timestamp;
+// 	}
+// }
+// const VERSION = getVersion();
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	preprocess: [
+// 		vitePreprocess(),
+// 		preprocess({
+// 			postcss: true,
+// 		}),
+// 	],
+
+// 	kit: {
+// 		// adapter: adapter(),
+// 		version: {
+// 			name: VERSION,
+// 		},
+// 		alias: {
+// 			'web-config': './src/web-config.json',
+// 			$external: './src/external',
+// 		},
+// 		serviceWorker: {
+// 			register: false,
+// 		},
+// 		paths: {
+// 			relative: true,
+// 		},
+// 		adapter: adapter({
+// 			runtime: 'nodejs18.x',
+// 		  }),
+// 	},
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		vitePreprocess(),
-		preprocess({
-			postcss: true,
-		}),
-	],
-
 	kit: {
-		// adapter: adapter(),
-		version: {
-			name: VERSION,
-		},
-		alias: {
-			'web-config': './src/web-config.json',
-			$external: './src/external',
-		},
-		serviceWorker: {
-			register: false,
-		},
-		paths: {
-			relative: true,
-		},
-		adapter: adapter({
-			runtime: 'nodejs18.x',
-		  }),
-	},
+		adapter: adapter()
+	}
 };
 
 export default config;
